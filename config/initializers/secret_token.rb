2f6021548d3f0ccb8f668ef4d8b9ed4933f26893
Isaac Cambron <icambron@gmail.com>
2013-07-26 18:01:04 -0400

    private secret_token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Wedding::Application.config.secret_token = 'ad599c700492884f2cdbb04fa1b3483d2ac90fcedf40e24be6b6ebc4769143011ea6abfa5f17bb5fbfb79e112ae3941dd0b4f29feb296272bcdc634785493628'
+Wedding::Application.config.secret_token = ENV["WEDDING_SECRET_TOKEN"]
